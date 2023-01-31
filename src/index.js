5c0fd8d1419467999f4998897616f69eca9d5a46
VladzimirBely <vladzimir.bely@gmail.com>
2023-01-31 19:55:18 +0300

    feat: create brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+
+  let checkStr = true;
+  
+  while (checkStr) {
+    let string = str;
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      let bracketsToStr = bracketsConfig[i].join('');
+      str = str.split(bracketsToStr).join('');
+    }
+    if (string === str) {
+      checkStr = false;
+    }
+  } 
+  return str === '';
 }
